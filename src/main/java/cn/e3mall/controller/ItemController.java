4e020d7dcf6ecb904f90221243b37fae5fbc7d1a
zhangkunfeng <zkf291990078@163.com>
2018-01-10 17:32:13 +0800

    初始化代码

---
+++ src/main/java/cn/e3mall/controller/ItemController.java
@@ -0,0 +1,27 @@
+package cn.e3mall.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import cn.e3mall.pojo.TbItem;
+import cn.e3mall.service.ItemService;
+
+@Controller
+public class ItemController {
+	
+	@Autowired
+	ItemService itemService;
+	
+	@RequestMapping("/item/{itemId}")
+	@ResponseBody
+
+	public TbItem getItemById(@PathVariable long itemId) {
+		TbItem tbItem = itemService.getItemById(itemId);
+		return tbItem;
+
+	}
+
+}
