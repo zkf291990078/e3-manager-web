6a00c9fd433d19016cc9be4c144661fe941dbc6e
zhangkunfeng <zkf291990078@163.com>
2018-01-10 18:24:05 +0800

    web添加静态资源，controller

--- src/main/java/cn/e3mall/controller/ItemController.java
+++ src/main/java/cn/e3mall/controller/ItemController.java
@@ -6,22 +6,29 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import cn.e3mall.common.pojo.EasyUIDataGridResult;
 import cn.e3mall.pojo.TbItem;
 import cn.e3mall.service.ItemService;
 
 @Controller
 public class ItemController {
-	
+
 	@Autowired
 	ItemService itemService;
-	
+
 	@RequestMapping("/item/{itemId}")
 	@ResponseBody
 
 	public TbItem getItemById(@PathVariable long itemId) {
 		TbItem tbItem = itemService.getItemById(itemId);
 		return tbItem;
+	}
 
+	@RequestMapping("/item/list")
+	@ResponseBody
+	public EasyUIDataGridResult getItemList(Integer page, Integer rows) {
+		EasyUIDataGridResult result = itemService.getItemList(page, rows);
+		return result;
 	}
 
 }
