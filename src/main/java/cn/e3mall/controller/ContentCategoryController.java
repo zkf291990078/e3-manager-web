06346540a6b545c2ae0469a416f467240fb1e239
zhangkunfeng <291990078@qq.com>
2018-01-17 00:38:13 +0800

    圖片上傳修改、內容分類controller提交

---
+++ src/main/java/cn/e3mall/controller/ContentCategoryController.java
@@ -0,0 +1,29 @@
+package cn.e3mall.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import cn.e3mall.common.pojo.EasyUITreeNode;
+import cn.e3mall.content.service.ContentCategoryService;
+
+@Controller
+public class ContentCategoryController {
+
+	@Autowired
+	private ContentCategoryService contentCategoryService;
+	
+	@RequestMapping("/content/category/list")
+	@ResponseBody
+	public List<EasyUITreeNode> getContentCatList(
+			@RequestParam(value="id", defaultValue="0") Long parentId) {
+		
+		List<EasyUITreeNode> list = contentCategoryService.getContentCategoryList(parentId);
+		return list;
+	}
+	
+}
